555e723bd1b6f1e6fb7c7481ef4918b10f9f5053
Michal <mkzaborowski@gmail.com>
2023-09-12 00:40:06 +0100

    node hw1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Repo link: https://github.com/MichalZaborowski91/goit-node-hw1
+
+Screenshots:
+
+List contacts: https://monosnap.com/file/Ju3XJyyidtUoKiimoTSFh16jvODi0g
+
+Get contact by id: https://monosnap.com/file/dKYzjU5DDpvHbCONNpWIG2bTzysj3w
+
+Add contact: https://monosnap.com/file/ihLYSQ6bnWd7dkIsUzobd5h5gf5oIm
+
+Delete contact: https://monosnap.com/file/r4rxTUjlWYoFZtBXhMMxUEt288Z4xu
--- contacts.js
+++ contacts.js
@@ -33,9 +33,49 @@ function getContactById(contactId) {
 }
 
 function removeContact(contactId) {
-  // ...twój kod
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) {
+      console.error("Error reading contacts.json file.");
+      return;
+    }
+    const contacts = JSON.parse(data);
+    const index = contacts.find((contact) => contact.id === contactId);
+
+    if (index === -1) {
+      console.error(`Contact under id: ${contactId} doesn't exist.`);
+      return;
+    }
+    const removedContact = contacts.splice(index, 1);
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (error) => {
+      if (error) {
+        console.error(`Error saving file`);
+        return;
+      }
+      console.log("Deleted contact:");
+      console.table(removedContact);
+    });
+  });
 }
 
 function addContact(name, email, phone) {
-  // ...twój kod
+  fs.readFile(contactsPath, (error, data) => {
+    if (error) {
+      console.error("Error reading contacts.json file.");
+      return;
+    }
+    const contacts = JSON.parse(data);
+    const newContact = { name, email, phone };
+    contacts.push(newContact);
+
+    fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (error) => {
+      if (error) {
+        console.error("Error saving file");
+        return;
+      }
+      console.log("Contact added:");
+      console.table(newContact);
+    });
+  });
 }
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
